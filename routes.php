e7d8b1ce6eccc9fef2a7300153b78afd8f399894
LuuKasBR <lsdo2011@gmail.com>
2020-02-06 19:37:36 -0300

    Ajustando nome da classe Controller

--- routes.php
+++ routes.php
@@ -8,15 +8,15 @@ $app->group('/v1', function() {
      * Dentro de v1, o recurso /book
      */
     $this->group('/book', function() { 
-        $this->get('', '\App\v1\Controllers\BookController:listBook');
-        $this->post('', '\App\v1\Controllers\BookController:createBook');
+        $this->get('', '\App\v1\Controllers\BooksController:listBook');
+        $this->post('', '\App\v1\Controllers\BooksController:createBook');
          
         /**
          * Validando se tem um integer no final da URL
          */
-        $this->get('/{id:[0-9]+}', '\App\v1\Controllers\BookController:viewBook');
-        $this->put('/{id:[0-9]+}', '\App\v1\Controllers\BookController:updateBook');
-        $this->delete('/{id:[0-9]+}', '\App\v1\Controllers\BookController:deleteBook');
+        $this->get('/{id:[0-9]+}', '\App\v1\Controllers\BooksController:viewBook');
+        $this->put('/{id:[0-9]+}', '\App\v1\Controllers\BooksController:updateBook');
+        $this->delete('/{id:[0-9]+}', '\App\v1\Controllers\BooksController:deleteBook');
     });
 
     /**
